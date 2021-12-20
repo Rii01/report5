b5253b1296bb306f015895b9eabe47a1e0a5af1d
根路銘理子 <riko_mogu@rikomogu.local>
2021-12-21 00:08:57 +0900

    step3

--- Report5.java
+++ Report5.java
@@ -1,11 +1,15 @@
 public class  Report5 {
     public static void main(String[] args) throws Exception {
-        try{    
+/*         try{    
             String str = null;
             System.out.println(str.length());
         } catch(NullPointerException e){
             System.out.println(e.getMessage());//エラーの詳細だけを出力する
             System.out.println("エラーになります。(NullPointerException)");
-        }
+        }*/ 
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(value);        
+        
     }
 }
