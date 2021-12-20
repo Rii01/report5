dcafeb26853466bd63f73677aaf79749710d9012
根路銘理子 <riko_mogu@rikomogu.local>
2021-12-21 00:05:12 +0900

    step2

--- Report5.java
+++ Report5.java
@@ -1,7 +1,11 @@
 public class  Report5 {
-    public static void main(String[] args) {
-  
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) throws Exception {
+        try{    
+            String str = null;
+            System.out.println(str.length());
+        } catch(NullPointerException e){
+            System.out.println(e.getMessage());//エラーの詳細だけを出力する
+            System.out.println("エラーになります。(NullPointerException)");
+        }
     }
 }
